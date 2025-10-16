90965bb7fcba8efab59a25fa2a1df19e835db1fc
omrmh <157282671+omrmh@users.noreply.github.com>
2025-10-16 08:08:29 +0300

    Create introduction.js

---
+++ introduction.js
@@ -0,0 +1,28 @@
+// ملف تعريف الطالب
+function getStudentName() {
+    // يجب على الطالب إرجاع اسمه الكامل هنا
+    return "ــــــــــــــــ";
+}
+
+function getStudentAge() {
+    // يجب على الطالب إرجاع عمره هنا (رقم)
+    return 0;
+}
+
+function getStudentMajor() {
+    // يجب على الطالب إرجاع تخصصه هنا
+    return "ــــــــــــــــ";
+}
+
+function getStudentHobby() {
+    // يجب على الطالب إرجاع هوايته المفضلة هنا
+    return "ــــــــــــــــ";
+}
+
+// لا تغير هذا السطر
+module.exports = {
+    getStudentName,
+    getStudentAge,
+    getStudentMajor,
+    getStudentHobby
+};
